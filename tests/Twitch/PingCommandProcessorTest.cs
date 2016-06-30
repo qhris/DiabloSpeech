917dba7b4220965f5f05424a45b0d3916f3ce814
Christoffer Hjalmarsson <b.christoffer.hjalmarsson@gmail.com>
2016-06-30 19:18:51 +0200

    Add missing file for ping command test.

---
+++ tests/Twitch/PingCommandProcessorTest.cs
@@ -0,0 +1,38 @@
+﻿using DiabloSpeech.Business.Twitch;
+using DiabloSpeech.Business.Twitch.Processors;
+using NUnit.Framework;
+using tests.Twitch.Mocks;
+
+namespace tests.Twitch
+{
+    public class PingCommandProcessorTest
+    {
+        PingCommandProcessor processor;
+        TwitchConnectionMock connection;
+
+        TwitchMessageData MessageData =>
+            new TwitchMessageData();
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            processor = new PingCommandProcessor();
+            connection = new TwitchConnectionMock("test", "#test");
+        }
+
+        [Test]
+        public void PingCommandThrowsWithoutConnection()
+        {
+            Assert.That(() => processor.Process(null, MessageData), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void PingCommandSendPongAndFlushes()
+        {
+            processor.Process(connection, MessageData);
+            Assert.That(connection.FiredCommands.Count, Is.EqualTo(2));
+            Assert.That(connection.FiredCommands[0], Is.EqualTo("PONG"));
+            Assert.That(connection.FiredCommands[1], Is.EqualTo("__FLUSH"));
+        }
+    }
+}
